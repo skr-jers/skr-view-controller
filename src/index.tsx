33e67b85b48493a73d5dc84addb64b712dc787be
Jonathan Ruiz <ruijo01@ruijo01.sekura.mx>
2022-06-09 16:14:54 -0500

    strict mode off

--- src/index.tsx
+++ src/index.tsx
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
+    <>
         <Provider store={store}>
             <StateController/>
         </Provider>
-    </React.StrictMode>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
